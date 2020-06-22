69570eee5d7f0d5fa603e1a03ccb6ccb9847a16d
Amr Elshenawy <amrsamir500@hotmail.com>
2020-06-21 21:54:06 -0700

    Basic functions and Operate

---
+++ script.js
@@ -0,0 +1,26 @@
+add = (a, b) => {
+    return a + b;
+}
+
+subtract = (a, b) => {
+    return (a) - (b);
+}
+
+multiply = (a, b) => {
+    return a * b;
+}
+
+divide = (a, b) => {
+    return a / b;
+}
+
+operate = (operation, a, b) => {
+    if(operation === "+")
+        add(a, b);
+    else if(operation === "-")
+        subtract(a, b);
+    else if(operation === "*")
+        multiply(a, b);
+    else if(operation === "/")
+        divide(a, b);
+}
