c7252c1041c214f4740a97bdb5eb54526e2bd2b6
Amr Elshenawy <amrsamir500@hotmail.com>
2020-06-22 22:20:05 -0700

    HTML/CSS added. Background img.

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-add = (a, b) => {
-    return a + b;
-}
+add = (a, b) => {return a + b;}
 
-subtract = (a, b) => {
-    return (a) - (b);
-}
+subtract = (a, b) => {return (a) - (b);}
 
-multiply = (a, b) => {
-    return a * b;
-}
+multiply = (a, b) => {return a * b;}
 
-divide = (a, b) => {
-    return a / b;
-}
+divide = (a, b) => {return a / b;}
 
 operate = (operation, a, b) => {
     if(operation === "+")
