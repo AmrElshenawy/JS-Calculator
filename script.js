9f7119c01d9cf1fb53cc8047294ddb7ac48480ba
Amr Elshenawy <amrsamir500@hotmail.com>
2020-06-24 23:51:33 -0700

    More work on the Display function

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
 
-let input = 0;
-// buttons.addEventListener('click', displayinput());
+let input;
+let data;
+let outdisplay = '';
+let number1 = 0;
+let number2 = 0;
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
+    data = e;    
     input = e.target.innerHTML;
     displayinput(input);
     });
   });
 
 displayinput = (input) => {
-    display.textContent = input;
+    if(number2 === 0 && data.target.classList[1] === 'center-numbers' && outdisplay.length <= 10){
+        outdisplay += input;
+        display.textContent = outdisplay;
+    }
+    if(data.target.classList[1] === 'operations'){
+        number1 = parseInt(outdisplay);
+        outdisplay = '';
+        outdisplay += input;
+        display.textContent = outdisplay;
+        outdisplay = '';
+    }
+    if(number1 !== 0 && data.target.classList[1] === 'center-numbers' && outdisplay.length <= 10 ){
+        outdisplay += input;
+        display.textContent = outdisplay;
+    }
 }
 
 
