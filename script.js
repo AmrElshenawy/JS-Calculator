4fd0c879cdcfce8f5e94a9025cf4c6d72e742c2b
Amr Elshenawy <NCL01379@murata.com>
2020-07-06 12:46:05 +0100

    Code formatting and deleted old master code

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 // CALCULATOR FUNCTIONS //=========================================================
 add = (a, b) => {return a + b;}
-
 subtract = (a, b) => {return (a) - (b);}
-
 multiply = (a, b) => {return a * b;}
-
 divide = (a, b) => {
     if(b == 0){
         return "Math Error!"
@@ -13,7 +10,6 @@ divide = (a, b) => {
         return a / b;
     }
 }
-
 operate = (operator, a, b) => {
     switch(operator){
         case '+':
@@ -76,7 +72,6 @@ buttons.forEach((button) => {
     }
     });
   });
-
 addValue = (input) => {
     if(solutionFound == true) clear();
 
@@ -109,7 +104,6 @@ addValue = (input) => {
     displayNum1.textContent = value1;
     displayNum2.textContent = value2;
 }
-
 selectOperator = (input) => {
     if(!num1Selected) return;
 
@@ -136,7 +130,6 @@ selectOperator = (input) => {
         solutionFound = false;
     }
 }
-
 findSolution = () => {
     if(num1Selected && operatorSelected && num2Selected){
         value1 = +value1;
@@ -156,7 +149,6 @@ findSolution = () => {
         }
     }
 }
-
 invert = () => {
     if(solutionFound){
         value1 *= -1;
@@ -175,7 +167,6 @@ invert = () => {
         displayNum1.textContent = value1;
     }
 }
-
 percentage = () => {
     if(solutionFound){
         value1 /= 100;
@@ -194,7 +185,6 @@ percentage = () => {
         displayNum1.textContent = value1.toFixed(4);
     }
 }
-
 clear = () => {
     num1Selected = false;
     num2Selected = false;
@@ -207,50 +197,3 @@ clear = () => {
     displayOperator.textContent = "";
     displayNum2.textContent = "";
 }
-
-/* 
-let 
-
-let outdisplay = '';
-let number1 = 0;
-let number2 = 0;
-
-
-displayinput = (input) => {
-    if(num1 === false && data.target.classList[1] === 'center-numbers' && outdisplay.length <= 8){
-        outdisplay += input;
-        display.textContent = outdisplay;
-    }
-    if(data.target.classList[1] === 'operations'){
-        num1 = true;
-        number1 = +outdisplay;
-        outdisplay = '';
-        outdisplay = input;
-        operator = input;
-        display.textContent = outdisplay;
-        outdisplay = '';
-    }
-    if(num1 === true && data.target.classList[1] === 'center-numbers' && outdisplay.length <= 8){
-        outdisplay += input;
-        number2 += +outdisplay;
-        display.textContent = outdisplay;
-    }
-    if(/* flag === true  && data.target.classList[1] === 'equals'){
-        flag = false;
-        number2 = parseInt(outdisplay);
-        switch(operator){
-            case '+':
-                display.textContent = add(number1, number2);
-                break;
-            case '-':
-                display.textContent = subtract(number1, number2);
-                break;
-            case '×':
-                display.textContent = multiply(number1, number2);
-                break;
-            case '÷':
-                display.textContent = divide(number1, number2);
-                break;
-        }
-    }   
-} */
